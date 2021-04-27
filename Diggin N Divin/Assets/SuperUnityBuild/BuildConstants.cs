806bd6e8be92504b48659903ff82132a5921e3dd
Adam <voidkin@gmail.com>
2021-04-26 21:30:32 -0400

    submission

---
+++ Diggin N Divin/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,41 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Release,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        macOS,
+        Linux,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x86,
+        macOS,
+        Linux_x64,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637550668442377349);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Release;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.None;
+}
+
